49aa468b7024f783d378d4cc133eb79aa2bb63e9
hamryt <rkdl0829@naver.com>
2023-08-28 22:07:48 +0900

    discovery 서버 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,9 +21,6 @@ repositories {
 extra["springCloudVersion"] = "2022.0.4"
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- src/main/kotlin/com/daehwa/discovery/DiscoveryApplication.kt
+++ src/main/kotlin/com/daehwa/discovery/DiscoveryApplication.kt
@@ -2,7 +2,10 @@ package com.daehwa.discovery
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
+@EnableEurekaServer
 @SpringBootApplication
 class DiscoveryApplication
 
